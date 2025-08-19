377e5df2c0f5ef712c856130f828a71b8a3a8e8e
Atanium <119530178+Atanium@users.noreply.github.com>
2025-08-19 17:58:01 +0400

    Create setup.sh

---
+++ .script/setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+sudo pacman -Syu
+sudo pacman -S --needed git base-devel nano brightnessctl river foot ttf-liberation ttf-dejavu fuzzel 
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+wget https://raw.githubusercontent.com/Atanium/dotfiles/refs/heads/main/.config/init
+mkdir -p ~/.config/river
+mkdir ~/.config/foot
+mkdir -p ~/.config/systemd/user
+cp init ~/.config/river/init
+rm init
+cp /etc/xdg/foot/foot.ini ~/.config/foot/foot.ini
+
+cat > "$HOME/.config/systemd/user/riverstartup.service" <<EOF
+[Unit]
+Description=Run river at user login
+After=default.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/river
+RemainAfterExit=true
+
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl --user enable riverstartup.service
+
+sudo cat > "/etc/modprobe.d/rtw8852be.conf" <<EOP
+options rtw89_pci disable_aspm_l1=y disable_aspm_l1ss=y
+options rtw89pci disable_aspm_l1=y disable_aspm_l1ss=y
+options rtw89_core disable_ps_mode=y
+options rtw89core disable_ps_mode=y
+EOP
+
+yay -S ungoogled-chromium-bin
